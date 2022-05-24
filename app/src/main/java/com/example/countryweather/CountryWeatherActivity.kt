e284f39183016c9f7997e621971d476aa67a9837
Walaa9999 <walaa.dweikat@harri.com>
2022-05-24 21:07:57 +0300

    add nav_graph

--- app/src/main/java/com/example/countryweather/CountryWeatherActivity.kt
+++ app/src/main/java/com/example/countryweather/CountryWeatherActivity.kt
@@ -22,7 +22,7 @@ class CountryWeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_weather)
-
+        supportActionBar?.hide()
         countriesViewModel.loadCountries()
 
         val list = ArrayList<Country>()
